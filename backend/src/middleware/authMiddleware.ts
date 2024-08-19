a0977f74f4067e2dacbcc1f065b3725c889b368f
fernandozambone <fernando.zambone@gmail.com>
2024-08-18 21:47:46 -0500

    Created middleware for token validation and endpoint

---
+++ backend/src/middleware/authMiddleware.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from "express";
+import { getAuth } from "firebase-admin/auth";
+
+export const verifyToken = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "No token provided." });
+  }
+
+  try {
+    const decodedToken = await getAuth().verifyIdToken(token);
+    req.user = decodedToken;
+    next();
+  } catch (error) {
+    const err = error as Error;
+    return res
+      .status(401)
+      .json({ message: "Invalid token", error: err.message });
+  }
+};
--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { loginUser } from "../controllers/authController";
+import { verifyToken } from "../middleware/authMiddleware";
 
 const router = Router();
 
 router.post("/login", loginUser);
+router.get("/validate-token", verifyToken, (req, res) => {
+  res.status(200).json({ message: "Token is valid", user: req.user });
+});
 
 export default router;
